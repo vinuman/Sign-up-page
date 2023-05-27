f20c933abe25521e2ce63ad54afc215d34d87dae
Vinu Cyril <vinucyril@gmail.com>
2023-05-27 22:18:33 +0530

    initialLoad

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { useRef, useEffect } from "react";
 
 const Nav = () => {
+  const buttonRef = useRef(null);
+  useEffect(() => {
+    buttonRef.current.click();
+  }, []);
   return (
     <nav>
       <p>Header</p>
       <div className="nav-end">
         <Link style={{ textDecoration: "none", color: "#fff" }} to="/">
           {" "}
-          <p>Sign Up</p>
+          <p ref={buttonRef}>Sign Up</p>
         </Link>
         <Link style={{ textDecoration: "none", color: "#fff" }} to="/profile">
           {" "}
